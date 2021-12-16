a2ddc61f3fc7bc782ba8d08e40ffde896b248330
ank090 <ankver090@gmail.com>
2021-12-16 10:28:49 +0530

    Added UI elements

---
+++ main.py
@@ -0,0 +1,27 @@
+from flask import Flask,url_for, redirect ,render_template,request
+import pickle
+from sklearn.tree import DecisionTreeRegressor
+from sklearn.preprocessing import StandardScaler
+
+
+app=Flask(__name__)
+reg = pickle.load(open('./static/regressor.pkl','rb'))
+sc = pickle.load(open('./static/scaler.pkl','rb'))
+result=None
+
+@app.route('/')
+def home(): 
+    return render_template('home.html')
+
+@app.route('/predict',methods=['GET','POST'])
+def predict():
+    if request.method=="POST":
+
+     result=reg.predict(sc.transform([[int(request.form.get("cylinder")),(16.387*int(request.form.get("disp"))),int(request.form.get("hp")),(2.205*int(request.form.get("weight"))),(0.618*int(request.form.get("acc"))),int(request.form.get("year")),int(request.form.get("origin"))]]))
+     return render_template('predict_page.html',Milage=(result*0.425143707))
+    return render_template('predict_page.html')
+
+    
+
+if __name__=="__main__":
+    app.run(debug=True)
